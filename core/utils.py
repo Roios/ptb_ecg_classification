b6fd19c33c2c2ba71f842035d9c36486d785ed4a
Roios <pedro.roios@gmail.com>
2024-07-18 13:08:10 +0200

    add data augmentation

--- core/utils.py
+++ core/utils.py
@@ -2,6 +2,8 @@ from collections import Counter
 from typing import Dict, List, Union
 
 import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MultiLabelBinarizer
 
 
 def calculate_distribution(labels: List, use_combo: bool = False) -> Union[Dict, Dict]:
@@ -21,3 +23,18 @@ def calculate_distribution(labels: List, use_combo: bool = False) -> Union[Dict,
     total_count = sum(class_counts.values())
     class_percentages = {cls: count / total_count * 100 for cls, count in class_counts.items()}
     return class_counts, class_percentages
+
+
+def binarize_labels(multi_labels: pd.Series) -> Union[np.ndarray, List]:
+    """ Binarize labels.
+
+    Args:
+        multi_labels (pd.Series): Labels to binarize
+
+    Returns:
+        Union[np.ndarray, List]: Binarized labels and classes
+    """
+    mlb = MultiLabelBinarizer()
+    mlb.fit(multi_labels)
+    bin_labels = mlb.transform(multi_labels)
+    return bin_labels, mlb.classes_
